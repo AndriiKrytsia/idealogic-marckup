f415a57bcf6482cff5f0110fd5591d6e0ca1a480
Andrii Krytsia <124679055+AndriiKrytsia@users.noreply.github.com>
2023-10-19 12:02:01 +0200

    888

---
+++ src/components/auth/login/LoginPage.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export const LoginPage = () => {
+  return (
+    <div>
+      <h1>Login PAGE</h1>
+      <form>
+        <input type="text" />
+        <input type="password" />
+      </form>
+    </div>
+  );
+};
